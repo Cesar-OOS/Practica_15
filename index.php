92653a0ea7d568be6eafe7faea7184af6a4820d3
Cesar-OOS <contactocoos@gmail.com>
2024-05-30 11:28:03 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,34 @@
+<?php
+function calcula_masa() {
+    $pi = 3.141592;
+    $constante = 6.67430E-11;
+    $distancia = 3.84E8;
+    $periodo = 2360448;
+    $primeraParteA = 4 * ($pi * $pi);
+    $segundaParteA = $distancia * $distancia * $distancia;
+    $parteA = $primeraParteA * $segundaParteA;
+    $segundaParteB = $periodo * $periodo;
+    $parteB = $constante * $segundaParteB;
+    $masa = $parteA / $parteB;
+    return $masa;
+}
+
+$masa_tierra = calcula_masa();
+?>
+
 <!DOCTYPE html>
 <html lang="es-mx">
 <head>
     <meta charset="UTF-8">
     <title>CiTIM Grupo XB</title>
-    <link rel="stylesheet" href="stem.css"/>
-
+    <link rel="stylesheet" href="css/stem.css"/>
     <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
-    <?php
-        function calcula_masa(){
-          $pi = 3.141592;
-          $constante = 6.67430E-11;
-          $distancia = 3.84E8;
-          $periodo = 2360448;
-          $primeraParteA = 4 * (pi * pi);
-          $segundaParteA = distancia * distancia * distancia;
-          $parteA = primeraParteA * segundaParteA;
-          $segundaParteB = periodo * periodo;
-          $parteB = constante * segundaParteB;
-          $masa = parteA / parteB;
-          return $masa;
-        }
-    ?>
 </head>
 <body>
   <section class="wrapper">
+    <header>
+      <h1 class="logo"><a href="stem.html">Evaluación</a></h1>
+    </header>
     <section id="contenedor">
       <section class="problema">
         <h2>Problema: Calcular la masa de la Tierra</h2>
@@ -33,34 +38,37 @@
       <section class="esquemaProblema">
         <h2>Esquema</h2>
         <center>
-        <img class="imgProblema" src="images/tierra.png">
+          <img class="imgProblema" src="images/tierra.png" alt="Esquema del problema">
         </center>
       </section>
       <section class="formulas">
         <h2>Fórmulas</h2>
-        G = Constante Gravitacional <br>
-        T = Periodo Orbital <br>
-        r = Distancia <br>
+        <p>G = Constante Gravitacional</p>
+        <p>T = Periodo Orbital</p>
+        <p>r = Distancia</p>
       </section>
       <section class="datos">
         <h2>Datos:</h2>
-        Periodo Orbital T = 27.32 días o 2360448 segundos. <br> 
-        Distancia = 3.84x10^8 metros. <br>
-        Constante Gravitacional = 6.67430×10^−11 m³ kg⁻¹ s⁻².
+        <p>Periodo Orbital T = 27.32 días o 2360448 segundos.</p>
+        <p>Distancia = 3.84×10^8 metros.</p>
+        <p>Constante Gravitacional = 6.67430×10^−11 m³ kg⁻¹ s⁻².</p>
       </section>
       <section class="calculos">
         <h2>Solución</h2>
-        <p>La masa de la Tierra es:<br>
-        Masa de la tierra = (4π^2 / G) * (r^3 / T^2)
+        <p>La masa de la Tierra es:</p>
+        <p>Masa de la tierra = (4π² / G) * (r³ / T²)</p>
       </section>
       <section class="resultado">
         <h2>Resultado:</h2>
-        <div id="resultadoA"></div>
-        <?php
-          echo "<h1> La tierra tiene una masa de:".calcula_masa()."kg </h1>";
-        ?>
+        <div id="resultadoA">
+            <?php
+            echo "La tierra tiene una masa de: " . number_format($masa_tierra, 2, '.', '') . " kg";
+            ?>
+        </div>
       </section>
     </section>
+    <footer class="pie">
+    </footer>
   </section>
 </body>
 </html>
